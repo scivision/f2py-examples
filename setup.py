6551ac2490df91a838bd82e0e493b07d9847d44e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-10-04 22:29:39 -0400

    auto-minGW

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import setuptools  # noqa: F401
 from numpy.distutils.core import setup, Extension
+from pathlib import Path
+import os
+
+
+if os.name == 'nt':
+    sfn = Path(__file__).parent / 'setup.cfg'
+    stxt = sfn.read_text()
+    if '[build_ext]' not in stxt:
+        with sfn.open('a') as f:
+            f.write("[build_ext]\ncompiler = mingw32")
 
 setup(ext_modules=[Extension(name='pyprod',
                              sources=['prod.f90'],
